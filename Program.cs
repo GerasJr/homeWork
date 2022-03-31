c32a34b2eccdc33dfd74d28965ce1324a7fe0446
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-03-31 19:42:28 +0300

    1
    1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            float speed = 1.5f;
+            bool isAlive = true;
+            byte bit = 1;
+            string text = "bla bla";
+            char a = 'a';
+            double seconds = 12.05;
+            decimal minute = 64.43m;
+            long overSpeed = 21535463457367;
+            short count = 123;
+        }
+    }
+}

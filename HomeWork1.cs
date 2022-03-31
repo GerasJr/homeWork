559e0949c540379b7502a25f8200cd6b2a023798
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-03-31 19:17:35 +0300

    1
    1

---
+++ HomeWork1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class HomeWork1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ЯЮниор");
+        }
+    }
+}

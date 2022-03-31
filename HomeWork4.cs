6fa9bae4ae10a44196045b809793445ab4a41da1
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-03-31 23:42:18 +0300

    4
    4

---
+++ HomeWork4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork4
+{
+    class HomeWork4
+    {
+        static void Main(string[] args)
+        {
+            string name = "Василев";
+            string secondName = "Ваня";
+
+            Console.WriteLine("Имя: " + name + " Фамилия: " + secondName);
+            var nameChange = name;
+            name = secondName;
+            secondName = nameChange;
+            Console.WriteLine("Имя: " + name + " Фамилия: " + secondName);
+        }
+    }
+}

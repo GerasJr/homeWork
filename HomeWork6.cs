f2d8edd325a5b2ca05c1e38d74acc20063027e91
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-01 23:21:45 +0300

    6
    6

---
+++ HomeWork6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork6
+{
+    class HomeWork6
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int picturesRange = 3;
+
+            int ranges = pictures / picturesRange;
+            int pictureRemainders = pictures % picturesRange;
+            Console.WriteLine("Картин - " + pictures);
+            Console.WriteLine("Всего рядов - " + ranges);
+            Console.WriteLine("Остаток - " + pictureRemainders);
+        }
+    }
+}

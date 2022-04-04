73fd9dbaac2619329e252aa7b75953cd949f178d
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-04 18:07:57 +0300

    Create HomeWork8.cs

---
+++ HomeWork8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace учусь
+{
+    class HomeWork8
+    {
+        static void Main(string[] args)
+        {
+            int grannys;
+            int receptionTime = 10;
+            int minutesToWait;
+            int hoursToWait;
+            int minutesInHour = 60;
+
+            Console.WriteLine("Введите кол-во старушек");
+            grannys = Convert.ToInt32(Console.ReadLine());
+            minutesToWait = grannys * receptionTime;
+            hoursToWait = minutesToWait / minutesInHour;
+            minutesToWait = minutesToWait - hoursToWait * minutesInHour;
+            Console.WriteLine("Вам нужно ждать " + hoursToWait + " час(а/ов) и " + minutesToWait + " минут.");
+        }
+    }
+}

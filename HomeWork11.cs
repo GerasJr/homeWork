626e603cd0d2e9289b7d23e096a60cec86d4fe16
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-09 21:31:23 +0300

    Create HomeWork11.cs

---
+++ HomeWork11.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12667
+{
+    class HomeWork11
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int finishNumber = 96;
+            int numberRise = 7;
+
+            Console.WriteLine(number);
+
+            while (number != finishNumber)
+            {
+                number += numberRise;
+                Console.WriteLine(number);
+            }
+        }
+    }
+}

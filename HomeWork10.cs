af65a3b559ef1d20cf8d6bf1cda9c44cfa3b9670
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-09 20:35:36 +0300

    Create HomeWork10.cs

---
+++ HomeWork10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork_10
+{
+    class HomeWork10
+    {
+        static void Main(string[] args)
+        {
+            int cycleNumber = 0;
+            string stopWord = "0";
+
+            while (stopWord != "exit")
+            {
+                ++cycleNumber;
+                Console.WriteLine($"Цикл - {cycleNumber}, что бы остановить цикл введите *exit*");
+                stopWord = Console.ReadLine();
+            }
+            Console.WriteLine("Цикл завершен");
+        }
+    }
+}

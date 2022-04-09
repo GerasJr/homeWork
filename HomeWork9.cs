08aab0789e07cfc66f79b3996593cefb768b578b
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-09 20:16:56 +0300

    Create HomeWork9.cs

---
+++ HomeWork9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cycle
+{
+    class HomeWork9
+    {
+        static void Main(string[] args)
+        {
+            string userMassage;
+            int cycleCount;
+
+            Console.Write("Введите сообщение которое хотите вывести: ");
+            userMassage = Console.ReadLine();
+            Console.Write("Введите сколько хотите вывести сообщений: ");
+            cycleCount = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < cycleCount; i++)
+            {
+                Console.WriteLine(userMassage);
+            }
+        }
+    }
+}

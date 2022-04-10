0a7d5d7a77b1dd0b2f1631e2c9d72281ad75d0c7
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-10 22:15:32 +0300

    Create HomeWork12.cs

---
+++ HomeWork12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace y4yc
+{
+    class HomeWork12
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            Random random = new Random();
+
+            number = random.Next(0, 101);
+
+            for(int i = number; i > 0; i--)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

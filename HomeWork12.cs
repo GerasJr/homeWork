2feea54bdd6eb8e4bbe6147ead045ca5a98694f6
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-10 22:42:52 +0300

    Update HomeWork12.cs

--- HomeWork12.cs
+++ HomeWork12.cs
@@ -7,13 +7,15 @@ namespace y4yc
         public static void Main(string[] args)
         {
             Random randomNumber = new Random();
-            int multiplicityFive = 5;
-            int multiplicityThree = 3;
+            int multiplicity0 = 5;
+            int multiplicity1 = 3;
+            int minValue = 0;
+            int maxValue = 101;
             int amount = 0;
 
-            for(int i = randomNumber.Next(0, 101); i > 0; i--)
+            for(int i = randomNumber.Next(minValue, maxValue); i > 0; i--)
             {
-                if(i % multiplicityThree == 0 || i % multiplicityFive == 0)
+                if(i % multiplicity1 == 0 || i % multiplicity0 == 0)
                 {
                     amount += i;
                 }

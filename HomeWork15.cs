7fce50cc8ec01a5810294fd6334860f5ecbf4541
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-12 20:34:00 +0300

    Create HomeWork15.cs

---
+++ HomeWork15.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace еааа
+{
+    class HomeWork15
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            char userSymbol;
+            int additionalSymbols = 2;
+
+            Console.WriteLine("Введите ваше имя");
+            userName = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            userSymbol = Convert.ToChar(Console.ReadLine());
+            
+            for(int i = 0; i < userName.Length + additionalSymbols; i++)
+            {
+                Console.Write(userSymbol);
+            }
+
+            Console.WriteLine("\n" + userSymbol + userName + userSymbol);
+
+            for (int i = 0; i < userName.Length + additionalSymbols; i++)
+            {
+                Console.Write(userSymbol);
+            }
+        }
+    }
+}

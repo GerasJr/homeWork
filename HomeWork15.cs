2ebcb44eeaaba9fc59ea71e3596857bdad1fa049
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-12 21:21:04 +0300

    Update HomeWork15.cs

--- HomeWork15.cs
+++ HomeWork15.cs
@@ -9,23 +9,16 @@ namespace еааа
             string userName;
             char userSymbol;
             int additionalSymbols = 2;
+            string userSymblols;
 
             Console.WriteLine("Введите ваше имя");
             userName = Console.ReadLine();
             Console.WriteLine("Введите символ");
             userSymbol = Convert.ToChar(Console.ReadLine());
-            
-            for(int i = 0; i < userName.Length + additionalSymbols; i++)
-            {
-                Console.Write(userSymbol);
-            }
-
-            Console.WriteLine("\n" + userSymbol + userName + userSymbol);
-
-            for (int i = 0; i < userName.Length + additionalSymbols; i++)
-            {
-                Console.Write(userSymbol);
-            }
+            userSymblols = new string(userSymbol, userName.Length + additionalSymbols);
+            Console.WriteLine(userSymblols);
+            Console.WriteLine(userSymbol + userName + userSymbol);
+            Console.WriteLine(userSymblols);
         }
     }
 }

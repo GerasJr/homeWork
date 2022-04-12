e980bacaa2256656d6c5f1b3ce7c578d3d64091e
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-12 21:56:06 +0300

    Create HomeWork16.cs

---
+++ HomeWork16.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ea_dz
+{
+    class HomeWork16
+    {
+        static void Main(string[] args)
+        {
+            string password = "Купил мужик шляпу а она ему как раз";
+            string userInput = "";
+            int trys = 3;
+
+            Console.WriteLine("Введите пароль");
+
+            while (trys > 0 && userInput != password)
+            {
+                Console.WriteLine("У вас " + trys + " попытки(ка)");
+                userInput = Console.ReadLine();
+
+                if(userInput == password)
+                {
+                    Console.WriteLine("Купил вторую а она ему как два");
+                }
+                else
+                {
+                    Console.WriteLine("Пароль не правильный");
+                    trys--;
+                }
+            }
+        }
+    }
+}

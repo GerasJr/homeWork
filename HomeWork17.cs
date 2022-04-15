a164d03b6a8ee214a75c3b356544c689a347a063
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-15 17:10:33 +0300

    Create HomeWork17.cs

---
+++ HomeWork17.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hm17
+{
+    class HomeWork17
+    {
+        static void Main(string[] args)
+        {
+            int numberN;
+            int numbersMultiplesN;
+            int minRangeN = 1;
+            int maxRangeN = 27;
+            int maxThreeDigit = 999;
+            int minThreeDigit = 100;
+            Random rangeOfN = new Random();
+
+            numberN = rangeOfN.Next(minRangeN, maxRangeN);
+            numbersMultiplesN = numberN;
+
+            for(int i = numberN; i <= maxThreeDigit; i++)
+            {
+                numbersMultiplesN += numberN;
+
+                if(numbersMultiplesN >= minThreeDigit && numbersMultiplesN <= maxThreeDigit)
+                {
+                    Console.WriteLine(numbersMultiplesN);
+                }
+            }
+        }
+    }
+}

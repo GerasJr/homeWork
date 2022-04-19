469c65d32caebbb8fff5843bd19212c45ee77657
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-19 17:59:13 +0300

    Create HomeWork21.cs

---
+++ HomeWork21.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace hm21
+{
+    class HomeWork21
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { {5, 75, 18, 3}, {83, 2, 4, 34 } };
+            int secondPostAmount = 0;
+            int firstPostMultiply = 1;
+            
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                secondPostAmount += array[1, i];
+            }
+
+            Console.WriteLine("Сумма второго столбца: " + secondPostAmount);
+
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                firstPostMultiply *= array[0, i];
+            }
+
+            Console.WriteLine("Произведение первого столбца: " + firstPostMultiply);
+        }
+    }
+}

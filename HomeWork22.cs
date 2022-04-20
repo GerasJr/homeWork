a3e38308792aee74e23fd300b6c5ff0b531f2385
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-20 16:45:23 +0300

    Create HomeWork22.cs

---
+++ HomeWork22.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace hm22
+{
+    class HomeWork22
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[10, 10];
+            int largestArray = 0;
+            Random random = new Random();
+            int minValue = 0;
+            int maxValue = 101;
+
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minValue, maxValue);
+                    Console.Write(array[i, j] + ", ");
+
+                    if(array[i, j] > largestArray)
+                    {
+                        largestArray = array[i, j];
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\n{largestArray}\n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if(array[i, j] == largestArray)
+                    {
+                        array[i, j] = 0;
+                    }
+
+                    Console.Write(array[i, j] + ", ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}

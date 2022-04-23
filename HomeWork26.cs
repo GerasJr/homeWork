039630dffff7738913377f3e490252eef4b559c5
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-23 16:36:07 +0300

    Create HomeWork26.cs

---
+++ HomeWork26.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace hm26
+{
+    class HomeWork26
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Random random = new Random();
+                int minValue = 0;
+                int maxValue = 100;
+                array[i] = random.Next(minValue, maxValue);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                int variableSwapper;
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        variableSwapper = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = variableSwapper;
+                    }
+                }
+            }
+
+            foreach(int number in array)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}

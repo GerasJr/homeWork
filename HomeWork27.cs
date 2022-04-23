d673bbacb31af67dbe74df769c62485d4c64ea00
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-23 17:09:33 +0300

    Create HomeWork27.cs

---
+++ HomeWork27.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hm27
+{
+    class HomeWork27
+    {
+        static void Main(string[] args)
+        {
+            string text = "Пельмени – знаменитое блюдо русской кухни, имеющее древние китайские, финно-угорские, тюркские и славянские корни.";
+            string[] words = text.Split(' ');
+
+            Console.WriteLine(text);
+
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}

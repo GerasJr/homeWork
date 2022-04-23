152ea7b52e094b240ae8770ec80c0c5ef4f0755d
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-23 12:41:56 +0300

    Create HomeWork24.cs

---
+++ HomeWork24.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace hm24
+{
+    class HomeWork24
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[0];
+            int[] tempNumbers = new int[0];
+            bool isCalculation = true;
+            string userInput;
+            int amount = 0;
+
+            while (isCalculation)
+            {
+                userInput = Console.ReadLine();
+
+                if(userInput != "sum" && userInput != "exit")
+                {
+                    tempNumbers = new int[numbers.Length + 1];
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        tempNumbers[i] = numbers[i];
+                    }
+
+                    tempNumbers[tempNumbers.Length - 1] = Convert.ToInt32(userInput);
+                    numbers = tempNumbers;
+                }
+                else if(userInput == "sum")
+                {
+                    for(int i = 0; i < numbers.Length; i++)
+                    {
+                        amount += numbers[i];
+                    }
+
+                    Console.WriteLine(amount);
+                    isCalculation = false;
+                }
+                else if(userInput == "exit")
+                {
+                    isCalculation = false;
+                }
+            }
+        }
+    }
+}

9d359566cbf7e756dbd533304eb124283161de8e
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-27 15:55:44 +0300

    Create HomeWork31.cs

---
+++ HomeWork31.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hm31
+{
+    class HomeWork31
+    {
+        static void Main(string[] args)
+        {
+            Conversion();
+        }
+
+        static int Conversion()
+        {
+            bool success = false;
+            int result = 0;
+
+            while (success == false)
+            {
+                Console.WriteLine("Введите число которое хотите сконвертировать");
+                string number = Console.ReadLine();
+                success = int.TryParse(number, out result);
+
+                if(success == false)
+                {
+                    Console.WriteLine("Конвертация не удалась");
+                }
+            }
+
+            Console.WriteLine($"Вы удачно сконвертировали число в int ({result})");
+            return result;
+        }
+    }
+}

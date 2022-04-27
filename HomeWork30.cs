7b86eac211a4e0389648d3f589107cbdc5e37ec4
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-27 15:12:43 +0300

    Create HomeWork30.cs

---
+++ HomeWork30.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hm30
+{
+    class HomeWork30
+    {
+        static void Main(string[] args)
+        {
+            DrawBar('#', 100f);
+        }
+
+        static void DrawBar(char symbol, float percent)
+        {
+            int fullGrid = 10;
+            float fullness;
+
+            fullness = percent / 10;
+            Console.Write("[");
+
+            for(int i = 0; i < fullGrid; i++)
+            {
+                if(i < fullness)
+                {
+                    Console.Write(symbol);
+                }
+                else
+                {
+                    Console.Write("_");
+                }
+            }
+
+            Console.WriteLine("]");
+        }
+    }
+}

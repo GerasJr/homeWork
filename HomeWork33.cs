5ee9876c2a141fad7a93885ec1599f62f5bb6b15
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-04-29 20:08:36 +0300

    Create HomeWork33.cs

---
+++ HomeWork33.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hm33
+{
+    class HomeWork33
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            Shuffle(array);
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+
+        static int[] Shuffle(int[] array)
+        {
+            int[] tempArray = new int[array.Length];
+            Random random = new Random();
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                int randomIndex = random.Next(0, array.Length);
+
+                tempArray[i] = array[i];
+                array[i] = array[randomIndex];
+                array[randomIndex] = tempArray[i];
+            }
+
+            return array;
+        }
+    }
+}

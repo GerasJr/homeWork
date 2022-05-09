d5f3aa019230eff23a9b15371b90a99a5be978cf
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-05-09 19:00:31 +0300

    Create HomeWork34.cs

---
+++ HomeWork34.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hm34
+{
+    class HomeWork34
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            bool isWorking = true;
+
+            dictionary.Add("Сталкер", "Человек, обладающий знанием территорий или сооружений");
+            dictionary.Add("Человек", "Представитель млекопитающих рода Homo отряда приматов, в узком смысле — вида Homo sapiens");
+            dictionary.Add("Нелюдь", "Плохой, дурной человек ");
+            dictionary.Add("Программист", "Разработчик программного обеспечения");
+            dictionary.Add("Юрист", "Специалист по правоведению, юридическим наукам");
+
+            while (isWorking)
+            {
+                Console.WriteLine("Введите слово:");
+                string userInput = Console.ReadLine();
+
+                if (dictionary.ContainsKey(userInput))
+                {
+                    Console.WriteLine(dictionary[userInput] + "\n");
+                }
+                else
+                {
+                    Console.WriteLine("Такого слова нет(\n");
+                }
+            }
+        }
+    }
+}

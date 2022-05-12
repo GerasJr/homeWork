f688d69e083ec931438096a39c31b399c9a341b6
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-05-12 14:46:10 +0300

    Create homeWork36.cs

---
+++ homeWork36.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hm36
+{
+    class homeWork36
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            bool isWork = true;
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Вводите числа через Enter");
+            Console.WriteLine("Что бы посчитать сумму всех чисел введите sum");
+            Console.WriteLine("Для выхода из программы введите exit");
+
+            while (isWork == true)
+            {
+                userInput = Console.ReadLine();
+
+                if(userInput == "sum")
+                {
+                    int sum = 0;
+
+                    foreach(var number in numbers)
+                    {
+                        sum += number;
+                    }
+
+                    Console.WriteLine(sum);
+                    isWork = false;
+                }
+                else if(userInput == "exit")
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    numbers.Add(Convert.ToInt32(userInput));
+                }
+            }
+        }
+    }
+}

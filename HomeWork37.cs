44e7263e078a7e3d9b8a1e4358e80b5b3668aaf3
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-05-12 16:12:44 +0300

    Create HomeWork37.cs

---
+++ HomeWork37.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hm37
+{
+    class HomeWork37
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dossiers = new Dictionary<string, string>();
+            bool isWork = true;
+
+            Console.WriteLine("1 - Добавить досье");
+            Console.WriteLine("2 - Вывести все досье");
+            Console.WriteLine("3 - Удалить досье");
+            Console.WriteLine("4 - Выход");
+
+            while (isWork)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Введите ФИО, потом через Enter должность");
+                        dossiers.Add(Console.ReadLine(), Console.ReadLine());
+                        Console.WriteLine("Досье успешно добавлено");
+                        break;
+                    case "2":
+                        foreach(var dossier in dossiers)
+                        {
+                            Console.WriteLine(dossier.Key + " - " + dossier.Value);
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите ФИО досье которого хотите удалить");
+                        string userInput = Console.ReadLine();
+
+                        if (dossiers.ContainsKey(userInput))
+                        {
+                            dossiers.Remove(userInput);
+                            Console.WriteLine("Досье успешно удалено");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Такого досье не найдено");
+                        }
+                        break;
+                    case "4":
+                        isWork = false;
+                        break;
+                }
+            }
+        }
+    }
+}

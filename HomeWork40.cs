bd5778c4472db7e806f728d2c04bf841232d01b0
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-05-16 18:34:46 +0300

    Create HomeWork40.cs

---
+++ HomeWork40.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace hm40
+{
+    class HomeWork40
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Renderer renderer = new Renderer();
+
+            player.Symbol = '@';
+            renderer.Render(player.PositionX, player.PositionY, player.Symbol);
+        }
+
+        public class Player
+        {
+            public char Symbol;
+            public int PositionX { get; private set; } = 5;
+            public int PositionY { get; private set; } = 10;
+        }
+        
+        public class Renderer
+        {
+            public void Render(int positionX, int positionY, char symbol)
+            {
+                Console.SetCursorPosition(positionX, positionY);
+                Console.WriteLine(symbol);
+            }
+        }
+    }
+}

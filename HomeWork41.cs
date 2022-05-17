872e6f0353b26201ad16e44c2bab1485b0d07230
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-05-17 18:26:06 +0300

    Create HomeWork41.cs

---
+++ HomeWork41.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hm41
+{
+    class HomeWork41
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Database
+    {
+        private Dictionary<int, Player> players = new Dictionary<int, Player>();
+        private int _minUnicueNumber = 0;
+
+        public void AddPlayer(string name, int level)
+        {
+            Player player = new Player(_minUnicueNumber, name, level);
+            players.Add(_minUnicueNumber, player);
+            _minUnicueNumber++;
+        }
+
+        public void BanPlayer(int unicueNumber)
+        {
+            if (players.ContainsKey(unicueNumber))
+            {
+                players[unicueNumber].IsBan = true;
+            }
+        }
+
+        public void UnbanPlayer(int unicueNumber)
+        {
+            if (players.ContainsKey(unicueNumber))
+            {
+                players[unicueNumber].IsBan = false;
+            }
+        }
+
+        public void DelitePlayer(int unicueNumber)
+        {
+            if (players.ContainsKey(unicueNumber))
+            {
+                players.Remove(unicueNumber);
+            }
+        }
+    }
+
+    class Player
+    {
+        private int _unicueNumber;
+        private string _name;
+        private int _level;
+        public bool IsBan;
+
+
+        public Player(int unicueNumber, string name, int level)
+        {
+            _name = name;
+            _level = level;
+            IsBan = false;
+        }
+    }
+}

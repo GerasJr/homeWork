be2598ca1543200f22531b552b351be134e7d5cf
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-05-20 15:42:18 +0300

    Update HomeWork42.cs

--- HomeWork42.cs
+++ HomeWork42.cs
@@ -8,7 +8,7 @@ namespace hm42
         static void Main(string[] args)
         {
             Player player = new Player();
-            player.Playing(true);
+            player.Play(true);
         }
     }
 
@@ -17,9 +17,8 @@ namespace hm42
         private List<Card> _cards = new List<Card>();
         private Deck _deck = new Deck();
 
-        public void Playing(bool isPlaying)
+        public void Play(bool isPlaying)
         {
-            _deck.DenoteCards();
             Console.WriteLine("Что бы взять карту введите 1");
             Console.WriteLine("Если хотите взять больше карт введите их кол-во");
             Console.WriteLine("Что бы вывести все ваши карты введите myCards");
@@ -30,7 +29,18 @@ namespace hm42
 
                 if(int.TryParse(userInput, out int number))
                 {
-                    GetCard(number);
+                    if(number <= _deck.ShowAllCards())
+                    {
+                        for (int i = 0; i < number; i++)
+                        {
+                            _cards.Add(_deck.GiveCard());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы пытаетесь взять слишком много карт");
+                        Console.WriteLine("Карт в колоде - " + _deck.ShowAllCards());
+                    }
                 }
                 else if(userInput == "myCards")
                 {
@@ -39,56 +49,66 @@ namespace hm42
             }
         }
 
-        private void GetCard(int amount)
-        {
-            for(int i = 0; i < amount; i++)
-            {
-                _deck.GiveCard(_cards);
-            }
-        }
-
         private void ShowAllCards()
         {
             for(int i = 0; i < _cards.Count; i++)
             {
-                Console.WriteLine(_cards[i].Name + " " + _cards[i].Suit);
+                Console.WriteLine(_cards[i].Rank + " " + _cards[i].Suit);
             }
         }
     }
 
     class Deck
     {
-        private string[] _suits = new string[] { "Пики", "Крести", "Черви", "Буби" };
-        private string[] _cardNames = new string[] { "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
         private List<Card> _cards = new List<Card>();
 
-        public void DenoteCards()
+        public Deck()
         {
-            for(int i = 0; i < _cardNames.Length; i++)
+            string[] _suits = new string[] { "Пики", "Крести", "Черви", "Буби" };
+            string[] _cardRanks = new string[] { "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+            int numberOfShuffles = 50;
+            Random random = new Random();
+
+            for (int i = 0; i < _cardRanks.Length; i++)
             {
                 for(int j = 0; j < _suits.Length; j++)
                 {
-                    Card card = new Card(_suits[j], _cardNames[i]);
+                    Card card = new Card(_suits[j], _cardRanks[i]);
                     _cards.Add(card);
                 }
             }
+
+            for (int i = 0; i < numberOfShuffles; i++)
+            {
+                int randomIndex = random.Next(0, _cards.Count);
+                Card tempCard = _cards[randomIndex];
+                _cards[randomIndex] = _cards[i];
+                _cards[i] = tempCard;
+            }
         }
 
-        public void GiveCard(List<Card> playerDeck)
+        public Card GiveCard()
         {
-            playerDeck.Add(_cards[playerDeck.Count]);
+            Card uppderCard = _cards[0];
+            _cards.RemoveAt(0);
+            return uppderCard;
+        }
+
+        public int ShowAllCards()
+        {
+            return _cards.Count;
         }
     }
 
     class Card
     {
         public string Suit { get; private set; }
-        public string Name { get; private set; }
+        public string Rank { get; private set; }
 
-        public Card(string suit, string cardName)
+        public Card(string suit, string cardRank)
         {
             Suit = suit;
-            Name = cardName;
+            Rank = cardRank;
         }
     }
 }

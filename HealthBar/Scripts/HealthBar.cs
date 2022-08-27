144e73f33b9911430c9470e673c74a4ad940126d
GerasJr <79013675+GerasJr@users.noreply.github.com>
2022-08-27 20:46:59 +0300

    1

---
+++ HealthBar/Scripts/HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    private Coroutine _changeValueJob;
+    private Slider _healthBar;
+    private float _changeRate = 150f;
+
+    private void Start()
+    {
+        _healthBar = GetComponent<Slider>();
+        _player.HealthChanged += StartChangeValue;
+    }
+
+    private IEnumerator ChangeValue()
+    {
+        while(_healthBar.value != _player.Health)
+        {
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _player.Health, _changeRate * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void StartChangeValue()
+    {
+        if (_changeValueJob != null)
+        {
+            StopCoroutine(_changeValueJob);
+        }
+
+        _changeValueJob = StartCoroutine(ChangeValue());
+    }
+}
---
+++ HealthBar/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _healthChanged;
+
+    public float Health { get; private set; } = 100;
+    private float _maxHealth = 100;
+    private float _minHealth = 0;
+    private BoxCollider2D _boxCollider2D;
+    private bool _isDead = false;
+
+    public event UnityAction HealthChanged
+    {
+        add => _healthChanged.AddListener(value);
+        remove => _healthChanged.RemoveListener(value);
+    }
+
+    private void Start()
+    {
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void Die()
+    {
+        _boxCollider2D.enabled = false;
+        _isDead = true;
+    }
+
+    public void Heal(float value)
+    {
+        if (_isDead == false)
+        {
+            Health = Mathf.Clamp(Health + value, _minHealth, _maxHealth);
+            _healthChanged?.Invoke();
+        }
+    }
+
+    public void Damage(float value)
+    {
+        if(_isDead == false)
+        {
+            Health = Mathf.Clamp(Health - value, _minHealth, _maxHealth);
+            _healthChanged?.Invoke();
+
+            if (Health == _minHealth)
+            {
+                Die();
+            }
+        }
+    }
+}
